4b096d9704559d3fcfbef16e085f31a0ffc8f383
Omar Ramadan <oramadan@fb.com>
2017-10-27 10:39:45 -0700

    Use no file extenstion for extra depmod override

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -2,7 +2,7 @@ DEPMOD_CONFIG_DIR=/etc/depmod.d/
 DEPMOD_CONFIG_FILE=${DEPMOD_CONFIG_DIR}/gtp.conf
 
 mkdir -p ${DEPMOD_CONFIG_DIR}
-echo "override gtp.ko * extra" >> ${DEPMOD_CONFIG_FILE}
+echo "override gtp * extra" >> ${DEPMOD_CONFIG_FILE}
 echo "override gtp.ko * weak-updates" >> ${DEPMOD_CONFIG_FILE}
 
 echo "Running depmod..."
